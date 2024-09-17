75fe59a4d77947410e3e526fb8fdc2cd233b79f6
devbenja <cariasbenjamin59@gmail.com>
2024-09-17 00:30:35 -0600

    Add: Create Endpoint to receive date and return unix y utc date

--- index.js
+++ index.js
@@ -10,6 +10,40 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/:date?', (req, res) => {
+
+    let dateParam = req.params.date;
+    let date;
+
+    if (!dateParam) {
+        
+        date = new Date();
+
+    } else {
+        
+        if (!isNaN(dateParam)) {
+            dateParam = parseInt(dateParam);
+        }
+
+        date = new Date(dateParam);
+
+    }
+
+    if (date.toString() === 'Invalid Date') {
+        
+        res.json({ error: 'Invalid Date' });
+
+    } else {
+       
+        res.json({
+            unix: date.getTime(),
+            utc: date.toUTCString()
+        });
+
+    }
+
+});
+
 app.listen(3000, () => {
-    console.log('Your App is Listening on Port:', 3000 );
+    console.log('Your App is Listening on Port:', 3000);
 });
