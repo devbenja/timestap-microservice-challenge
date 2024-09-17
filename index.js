0d3e02c33ca8918e5df7cf76e12d8699650d1598
devbenja <cariasbenjamin59@gmail.com>
2024-09-16 23:54:46 -0600

    Add: Send Html File to "/"

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Holi');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('Your App is Listening on Port:', 3000 );
+});
